353f8d5a900b4a53f6a2c901cf96ecbc1504bc18
sergio3144 <gonzalessergio3144@gmail.com>
2022-06-02 22:07:30 -0500

    Listo código limpio en JS

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ const $cajaDiv = document.querySelector('.caja')
 const $btnAumentar = document.querySelector('.btnAumentar');
 const $btnReset = document.querySelector('.btnReset');
 const $btnDisminuir = document.querySelector('.btnDisminuir');
+const $container = document.querySelector('.container')
 let contador = 0;
-$btnAumentar.addEventListener('click',()=>{
-    contador = contador + 1;
-    $cajaDiv.innerHTML = contador;
+$container.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('btnAumentar')){
+        contador = contador + 1;
+        $cajaDiv.textContent = contador;
+    }
+    if(e.target.classList.contains('btnReset')){
+        contador = 0;
+        $cajaDiv.textContent = contador
+    } 
+    if(e.target.classList.contains('btnDisminuir')) {
+        contador --;
+        $cajaDiv.textContent = contador;
+    } 
 })
-$btnReset.addEventListener('click',()=>{
-    contador = 0
-    $cajaDiv.innerHTML = contador
-})
-$btnDisminuir.addEventListener('click',()=>{
-    contador --;
-    $cajaDiv.innerHTML = contador;
-})
+
